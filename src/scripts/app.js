946eac5e4bcd1d0f994a04643656ad46d43e3ead
Carla Martin <carlamartinbenito@gmail.com>
2024-01-12 18:52:06 +0100

    Second optimization of all content according to PageSpeed Insights

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,12 @@
 //configuración del slick carousel en index.html según pantallas
 
 $('.carousel').slick({
-    dots: true,
-    speed: 300,
+    speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     arrows: false,
-    infinite: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     lazyLoad: 'ondemand',
     responsive: [
       {
@@ -16,7 +14,6 @@ $('.carousel').slick({
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          autoplaySpeed: 2000,
         }
       },
       {
